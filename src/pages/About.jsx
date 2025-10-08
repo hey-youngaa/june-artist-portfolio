2f612a6a3cd6ba1e024355ee49d8528cff0e4b5e
Jessica Choi <jessicahchoi93@gmail.com>
2025-10-08 14:54:30 -0400

    Removed toggle feature to fill up empty space on About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,13 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { introText } from '../data/AboutText';
 import { essayContent } from '../data/AboutText';
-import { references } from '../data/AboutText';
+import { footnotes } from '../data/AboutText';
 import { citations } from '../data/AboutText';
-import { IoMdArrowDropdown, IoMdArrowDropup} from "react-icons/io";
 
 const About = () => {
-  const [isEssayOpen, setIsEssayOpen] = useState(false);
-  const toggleEssay = () => setIsEssayOpen(!isEssayOpen);
 
   return (
-    <div>
+    <div className='about-page'>
       {/* Introduction */}
       <div className="intro">
         <p>{introText}</p>
@@ -18,48 +15,47 @@ const About = () => {
 
       {/* Essay */}
       <div className="essay">
-        <div className="essay-header" onClick={toggleEssay}>
+        <div className="essay-header">
           <h2>unable to contain myself: creation about object feeling (May 2024)</h2>
-          {isEssayOpen ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
         </div>
 
-        {isEssayOpen && (
-          <div className="essay-content">
-            {essayContent.map((item, index) => {
-              if (item.type === "paragraph"){
-                const parts = [];
-                let lastIndex = 0;
+        <div className="essay-content">
+          {essayContent.map((item, index) => {
+            if (item.type === "paragraph"){
+              const parts = [];
+              let lastIndex = 0;
 
-                item.references?.forEach(({index, ref}, i) => {
-                  parts.push(item.text.slice(lastIndex, index));
-                  parts.push(<sup key={i}>{ref}</sup>);
-                  lastIndex = index;
-                });
+              item.references?.forEach(({index, ref}, i) => {
+                parts.push(item.text.slice(lastIndex, index));
+                parts.push(<sup key={i}>{ref}</sup>);
+                lastIndex = index;
+              });
 
-                parts.push(item.text.slice(lastIndex));
-                return <p key={index}>{parts}</p>;
-              }
-              if (item.type === "blockquote"){
-                return <blockquote key={index}>{item.text}</blockquote>;
-              }
-              return null;
-            })}
+              parts.push(item.text.slice(lastIndex));
+              return <p key={index}>{parts}</p>;
+            }
+            if (item.type === "blockquote"){
+              return <blockquote key={index}>{item.text}</blockquote>;
+            }
+            return null;
+          })}
+            
+          <hr />
 
-            {/* End notes */}
-            <ol className="essay-references">
-              {references.map((ref, index) => (
-                <li key={index}>{ref}</li>
-              ))}
-            </ol>
+          {/* End notes */}
+          <ol className="essay-footnotes">
+            {footnotes.map((ref, index) => (
+              <li key={index}>{ref}</li>
+            ))}
+          </ol>
 
-            {/* Bibliography */}
-            <ul className="essay-citations">
-              {citations.map((cite, index) => (
-                <li key={index}>{cite}</li>
-              ))}
-            </ul>
-          </div>
-        )}
+          {/* Bibliography */}
+          <ul className="essay-citations">
+            {citations.map((cite, index) => (
+              <li key={index}>{cite}</li>
+            ))}
+          </ul>
+        </div>        
 
       </div>
 
